387a47fe44bbb8266be039abfe112421869d6aaf
Suresh Melvin Sigera <sureshsigera@gmail.com>
2022-08-03 09:54:17 -0400

    add more comments for better readability

--- js/main.js
+++ js/main.js
@@ -1,13 +1,18 @@
 class TicTacToe {
     constructor() {
+        // instance variables
         this.gridButtons = document.querySelectorAll("a");   // get all the grid buttons
         this.winMessageDiv = document.querySelector("#message"); // get win message div
         this.countClicks = 0;
     }
 
     setClickListenerAndLoadGame() {
+        // must use arrow function to prevent
+        // "Potentially invalid reference access to a class field via 'this.' of a nested function" warning message.
         this.gridButtons.forEach(currentBox => {
             let buttonStyle = currentBox.style;
+            // must use arrow function to prevent
+            // "Potentially invalid reference access to a class field via 'this.' of a nested function" warning message.
             currentBox.addEventListener('click', () => {
                 if (this.countClicks % 2 === 0 && buttonStyle.backgroundColor !== 'red' && buttonStyle.backgroundColor !== 'blue') {
                     console.log(this.countClicks % 2 === 0);
@@ -25,6 +30,7 @@ class TicTacToe {
     }
 
 
+    // instance method
     pickWinner() {
         /**
          * Algorithm to detect game winner
@@ -36,6 +42,7 @@ class TicTacToe {
          */
         if (this.gridButtons[0].style.backgroundColor === this.gridButtons[1].style.backgroundColor && this.gridButtons[1].style.backgroundColor === this.gridButtons[2].style.backgroundColor && this.gridButtons[0].style.backgroundColor !== "") {
             this.winMessageDiv.innerHTML = `${this.gridButtons[0].style.backgroundColor} player won the game`;
+            // since reloadGame() is a static method, we need to use className.methodName() to call it.
             TicTacToe.reloadGame();
         } else if (this.gridButtons[3].style.backgroundColor === this.gridButtons[4].style.backgroundColor && this.gridButtons[4].style.backgroundColor === this.gridButtons[5].style.backgroundColor && this.gridButtons[3].style.backgroundColor !== "") {
             this.winMessageDiv.innerHTML = `${this.gridButtons[3].style.backgroundColor} player won the game`;
@@ -64,6 +71,7 @@ class TicTacToe {
         }
     }
 
+    // static method to reset the game
     static reloadGame() {
         // https://stackoverflow.com/questions/2787679/how-to-reload-page-every-5-seconds
         setTimeout(function () {
@@ -72,5 +80,7 @@ class TicTacToe {
     }
 }
 
+// create new TicTacToe object
 let game = new TicTacToe();
+// call setClickListenerAndLoadGame();
 game.setClickListenerAndLoadGame();
